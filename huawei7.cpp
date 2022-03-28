f88a5b07d6369cc612b26e7a35b681ad53a049dc
Young Lee <72334951+Liesy@users.noreply.github.com>
2022-03-28 23:48:44 +0800

    2022.3.28

---
+++ huawei7.cpp
@@ -0,0 +1,22 @@
+//华为机试 https://www.nowcoder.com/ta/huawei
+
+#include <bits/stdc++.h>
+
+#define mem(f, v) memset(f, v, sizeof(f))
+
+typedef long long ll;
+const int N = 2;
+const int inf = 0x3f3f3f3f;
+const ll linf = 0x3f3f3f3f3f3f3f3fll;
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+
+    float num;
+    cin >> num;
+    cout << (int)(num + 0.5);
+
+    return 0;
+}
